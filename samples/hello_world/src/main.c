bb561288c93248c3d0e994131e5362aa2f5fa00b
Peter Bigot <peter.bigot@nordicsemi.no>
2019-09-19 13:08:43 -0500

    HACK: performance stress-test of timer API.
    This commit replaces hello_world with an application that initializes
    a set of timers at 1 ms intervals, lets them all run for nominally 3
    s, then stops them and measures the evenness of load, elapsed time,
    and callback rate.
    
    It is intended to provide validation and performance/space metrics for
    PR #17155.
    
    The initial conclusions using nrf52840_pca10056 are:
    * The code size for non-64-bit timer decreased slightly with the new
      implementation.
    * The RAM usage is unchanged for 32-bit timers, but increases by 64
      bytes per timer for 64-bit timers.
    * The new implementation is slightly slower than the existing one
    * The maximum number of active timers before the system destabilizes
      is decreased in the new implementation.
    * Tests with certain numbers of timers (e.g. 30) and certain
      configurations (e.g. non-legacy 32-bit) will sometimes fail to
      terminate, suggesting a bug in the timer handling.
    
    At 50 on upstream/master
       text	   data	    bss	    dec	    hex	filename
      12052	    672	   6357	  19081	   4a89	build/zephyr/zephyr.elf
    ***** Booting Zephyr OS build zephyr-v2.0.0-543-g296f92276fd1 *****
    Running with 50 timers, 32768 Hz
    148851 incrs in [2975, 2979] over 98384 cycles = 3002441 us
    49576 incrs/s
    
    At 50 on pr/17155
    # CONFIG_SYS_TIMEOUT_64BIT is not set
    # CONFIG_SYS_TIMEOUT_LEGACY_API is not set
       text	   data	    bss	    dec	    hex	filename
      11868	    672	   6358	  18898	   49d2	build/zephyr/zephyr.elf
    ***** Booting Zephyr OS build zephyr-v2.0.0-525-gb680b1e66773 *****
    Running with 50 timers, 32768 Hz
    146260 incrs in [2917, 2933] over 98364 cycles = 3001831 us
    48723 incrs/s
    
    At 50 on pr/17155
    # CONFIG_SYS_TIMEOUT_64BIT is not set
    CONFIG_SYS_TIMEOUT_LEGACY_API=y
       text	   data	    bss	    dec	    hex	filename
      11876	    672	   6358	  18906	   49da	build/zephyr/zephyr.elf
    ***** Booting Zephyr OS build zephyr-v2.0.0-525-gb680b1e66773 *****
    Running with 50 timers, 32768 Hz
    145739 incrs in [2897, 2927] over 98517 cycles = 3006500 us
    48474 incrs/s
    
    At 50 on pr/17155
    CONFIG_SYS_TIMEOUT_64BIT=y
       text	   data	    bss	    dec	    hex	filename
      12100	    672	   6982	  19754	   4d2a	build/zephyr/zephyr.elf
    ***** Booting Zephyr OS build zephyr-v2.0.0-525-gb680b1e66773 *****
    Running with 50 timers, 32768 Hz
    336748 incrs in [5607, 6865] over 257262 cycles = 7851013 us
    42892 incrs/s
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -7,7 +7,81 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 
+#define NUM_TIMERS 50
+#define TIMEOUT_INTERVAL K_MSEC(1)
+
+struct state_s {
+	struct k_timer timer;
+	volatile unsigned int state;
+	volatile unsigned int count;
+};
+
+struct state_s state[NUM_TIMERS];
+
+void expiry(struct k_timer *timer)
+{
+	struct state_s *sp = CONTAINER_OF(timer, struct state_s, timer);
+
+	sp->count += 1;
+	if (sp->state) {
+		k_timer_stop(timer);
+		sp->state += 1;
+	}
+}
+
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	const unsigned int cps = sys_clock_hw_cycles_per_sec();
+
+	printk("Running with %u timers, %u Hz\n", NUM_TIMERS, cps);
+
+	for (int i = 0; i < NUM_TIMERS; ++i) {
+		struct state_s *sp = state + i;
+
+		k_timer_init(&sp->timer, expiry, NULL);
+		sp->state = sp->count = 0;
+	}
+
+	unsigned int t0 = k_cycle_get_32();
+	for (int i = 0; i < NUM_TIMERS; ++i) {
+		struct state_s *sp = state + i;
+
+		k_timer_start(&sp->timer, K_NO_WAIT, TIMEOUT_INTERVAL);
+	}
+	k_sleep(3000);
+
+	for (int i = 0; i < NUM_TIMERS; ++i) {
+		struct state_s *sp = state + i;
+
+		sp->state = 1;
+	}
+
+	for (int i = 0; i < NUM_TIMERS; ++i) {
+		struct state_s *sp = state + i;
+		while (sp->state < 2) {
+			k_sleep(0);
+		}
+	}
+	unsigned int t1 = k_cycle_get_32();
+
+	unsigned int incrs = 0;
+	unsigned int min_incrs;
+	unsigned int max_incrs;
+	for (int i = 0; i < NUM_TIMERS; ++i) {
+		struct state_s *sp = state + i;
+
+		if ((i == 0) || (sp->count < min_incrs)) {
+			min_incrs = sp->count;
+		}
+		if ((i == 0) || (sp->count > max_incrs)) {
+			max_incrs = sp->count;
+		}
+		//printk("T%u incr %u\n", i, sp->count);
+		incrs += sp->count;
+	}
+
+	u32_t dur_us = (u32_t)((1000000ULL * (t1 - t0)) / cps);
+	printk("%u incrs in [%u, %u] over %u cycles = %u us\n", incrs,
+	       min_incrs, max_incrs, t1 - t0, dur_us);
+	printk("%u incrs/s\n", (u32_t)(incrs * 1000000ULL / dur_us));
 }
