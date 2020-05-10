aa41d6aed41b0d237f8dac003cf5ed6d6844f816
Peter A. Bigot <pab@pabigot.com>
2020-05-09 19:29:55 -0500

    WIP beacon: switch to extended advertising
    This is supposed to enable a limit on transmissions, and a callback
    when the last transmission completes.
    
    Fails because BT_HCI_OP_LE_SET_EXT_ADV_PARAM (where the transmission
    limit is provided) isn't implemented so bt_le_ext_adv_create() returns
    -EIO.
    
    Fix that with CONFIG_BT_CTLR_ADV_EXT=y.
    
    Then it doesn't build.
    
    Signed-off-by: Peter A. Bigot <pab@pabigot.com>

--- samples/bluetooth/beacon/src/main.c
+++ samples/bluetooth/beacon/src/main.c
@@ -22,23 +22,58 @@
  * https://github.com/google/eddystone/blob/master/protocol-specification.md
  * https://github.com/google/eddystone/tree/master/eddystone-url
  */
-static const struct bt_data ad[] = {
-	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-	BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0xaa, 0xfe),
-	BT_DATA_BYTES(BT_DATA_SVC_DATA16,
+
+static uint8_t ad_url[] = {
 		      0xaa, 0xfe, /* Eddystone UUID */
 		      0x10, /* Eddystone-URL frame type */
 		      0x00, /* Calibrated Tx power at 0m */
 		      0x00, /* URL Scheme Prefix http://www. */
 		      'z', 'e', 'p', 'h', 'y', 'r',
 		      'p', 'r', 'o', 'j', 'e', 'c', 't',
-		      0x08) /* .org */
+		      0x08, /* .org */
 };
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0xaa, 0xfe),
+	{
+		.type = BT_DATA_SVC_DATA16,
+		.data_len = sizeof(ad_url),
+		.data = ad_url,
+	},
+};
+static uint8_t *plc = ad_url + 5;
 
 /* Set Scan Response data */
 static const struct bt_data sd[] = {
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 };
+
+static void sent(struct bt_le_ext_adv *adv,
+		 struct bt_le_ext_adv_sent_info *info)
+{
+	printk("sent\n");
+}
+
+static void connected(struct bt_le_ext_adv *adv,
+		       struct bt_le_ext_adv_connected_info *info)
+{
+	printk("connected\n");
+}
+
+static void scanned(struct bt_le_ext_adv *adv,
+		    struct bt_le_ext_adv_scanned_info *info)
+{
+	printk("scanned\n");
+}
+
+static const struct bt_le_ext_adv_cb adv_callbacks = {
+	.sent = sent,
+	.connected = connected,
+	.scanned = scanned,
+};
+static struct bt_le_ext_adv *adv;
+static uint8_t adv_index;
 
 static void bt_ready(int err)
 {
@@ -50,8 +85,36 @@ static void bt_ready(int err)
 	printk("Bluetooth initialized\n");
 
 	/* Start advertising */
-	err = bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad),
+	struct bt_le_adv_param param = *
+		BT_LE_ADV_PARAM(BT_LE_ADV_OPT_USE_IDENTITY,
+				BT_GAP_ADV_SLOW_INT_MIN,
+				BT_GAP_ADV_SLOW_INT_MAX,
+				NULL);
+	printk("options %x, plc %c%c%c\n", param.options,
+	       plc[0], plc[1], plc[2]);
+#if 0
+	err = bt_le_adv_start(&param, ad, ARRAY_SIZE(ad),
 			      sd, ARRAY_SIZE(sd));
+#else
+	struct bt_le_ext_adv_start_param as_param = {
+		.num_events = 5,
+	};
+
+	err = bt_le_ext_adv_create(&param, &adv_callbacks, &adv);
+	printk("leac %d\n", err);
+	if (err == 0) {
+		adv_index = bt_le_ext_adv_get_index(adv);
+		printk("%p at %u\n", adv, adv_index);
+		err = bt_le_ext_adv_set_data(adv, ad, ARRAY_SIZE(ad),
+					     sd, ARRAY_SIZE(sd));
+		printk("leasd %d\n", err);
+	}
+	if (err == 0) {
+		err = bt_le_ext_adv_start(adv, &as_param);
+		printk("leas %d\n", err);
+	}
+#endif
+
 	if (err) {
 		printk("Advertising failed to start (err %d)\n", err);
 		return;
