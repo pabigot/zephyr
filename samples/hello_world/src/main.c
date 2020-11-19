230c7fb202e9a3ecc0ad2f73bdf5a8f2a4858d2c
Peter Bigot <peter.bigot@nordicsemi.no>
2020-11-19 11:06:12 -0600

    DNM hello_world: demonstrate thread issue with qemu_x86_64
    qemu_x86_64 appears to handle semaphores differently with respect to
    counting and impact on ready but not active threads.
    
    Running this with qemu_x86:
    
    *** Booting Zephyr OS build zephyr-v2.4.0-1701-g414ba6b0741e  ***
    Main thread prio: -1
    main giving 1
    main back 1
    main giving 2
    main back 2
    main giving 3
    main back 3
    coop_main 0x105000 at prio -3  <- coop_main doesn't start until main yields
    coop_main blocking
    coop_main took: 0
    coop_main blocking
    
    Running this with qemu_x86_64:
    
    *** Booting Zephyr OS build zephyr-v2.4.0-1701-g414ba6b0741e  ***
    Main thread prio: -1
    coop_main 0x105000 at prio -3
    main giving 1       <- coop_main gets to run without main yield
    coop_main blocking
    main back 1
    main giving 2
    main back 2
    main giving 3
    coop_main took: 0   <- coop_main gets to run without main yield
    main back 3
    coop_main blocking
    coop_main took: 0   <- coop_main gets to run again?
    coop_main blocking
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -7,7 +7,39 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 
+#define STACK_SIZE (512 + CONFIG_TEST_EXTRA_STACKSIZE)
+
+#define COOP_PRIORITY K_PRIO_COOP(1) /* = -3 */
+
+static K_SEM_DEFINE(sync, 0, 1);
+
+static void coop_main(void *p1, void *p2, void *p3)
+{
+	printk("coop_main %p at prio %d\n",
+	       k_current_get(), k_thread_priority_get(k_current_get()));
+	while (true) {
+		printk("coop_main blocking\n");
+
+		int rc = k_sem_take(&sync, K_FOREVER);
+
+		printk("coop_main took: %d\n", rc);
+	}
+}
+
+static K_THREAD_DEFINE(coop_thr, STACK_SIZE,
+		       coop_main, NULL, NULL, NULL,
+		       COOP_PRIORITY, 0, 0);
+
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	int ctr = 0;
+
+	printk("Main thread prio: %d\n",
+	       k_thread_priority_get(k_current_get()));
+
+	while (++ctr <= 3) {
+		printk("main giving %d\n", ctr);
+		k_sem_give(&sync);
+		printk("main back %d\n", ctr);
+	}
 }
