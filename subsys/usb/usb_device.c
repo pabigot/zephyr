e0c9c9d9537af361d23be0ba3c1fd1957f6b3dbd
Jennifer Williams <jennifer.m.williams@intel.com>
2021-03-10 06:29:55 +0200

    usb: usb_device: fix missing final else
    set_endpoint() and reset_endpoint() were missing final
    else statement in the if else if construct. This commit
    adds a final else {} with assert to comply with coding
    guideline 15.7.
    
    Signed-off-by: Jennifer Williams <jennifer.m.williams@intel.com>

--- subsys/usb/usb_device.c
+++ subsys/usb/usb_device.c
@@ -496,6 +496,8 @@ static bool set_endpoint(const struct usb_ep_descriptor *ep_desc)
 	} else if (ret) {
 		LOG_ERR("Failed to configure endpoint 0x%02x", ep_cfg.ep_addr);
 		return false;
+	} else {
+		; /* Final statement (see coding guideline 15.7) */
 	}
 
 	ret = usb_dc_ep_enable(ep_cfg.ep_addr);
@@ -504,6 +506,8 @@ static bool set_endpoint(const struct usb_ep_descriptor *ep_desc)
 	} else if (ret) {
 		LOG_ERR("Failed to enable endpoint 0x%02x", ep_cfg.ep_addr);
 		return false;
+	} else {
+		; /* Final statement (see coding guideline 15.7) */
 	}
 
 	usb_dev.configured = true;
@@ -540,6 +544,8 @@ static bool reset_endpoint(const struct usb_ep_descriptor *ep_desc)
 	} else if (ret) {
 		LOG_ERR("Failed to disable endpoint 0x%02x", ep_cfg.ep_addr);
 		return false;
+	} else {
+		; /* Final statement (see coding guideline 15.7) */
 	}
 
 	return true;
