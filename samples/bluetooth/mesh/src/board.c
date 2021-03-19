43be93fec5f3e50ebfcb3650ab4f5f218742ccf7
Peter Bigot <peter.bigot@nordicsemi.no>
2021-03-19 07:35:09 -0500

    fixup 33476
    Improve identification of available LED and button to make this usable
    on a wider range of boards.  Aliases remain the most common way of
    identifying generic resources.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- samples/bluetooth/mesh/src/board.c
+++ samples/bluetooth/mesh/src/board.c
@@ -10,20 +10,37 @@
 #include <drivers/gpio.h>
 #include "board.h"
 
-#if DT_NODE_EXISTS(DT_NODELABEL(led0))
-
+/* Locate led0 as alias or label by that name */
+#if DT_NODE_EXISTS(DT_ALIAS(led0))
+#define LED0 DT_ALIAS(led0)
+#elif DT_NODE_EXISTS(DT_NODELABEL(led0))
 #define LED0 DT_NODELABEL(led0)
+#else
+#define LED0 DT_INVALID_NODE
+#endif
+
+/* Locate button0 as alias or label by sw0 or button0 */
+#if DT_NODE_EXISTS(DT_ALIAS(sw0))
+#define BUTTON0 DT_ALIAS(sw0)
+#elif DT_NODE_EXISTS(DT_ALIAS(button0))
+#define BUTTON0 DT_ALIAS(button0)
+#elif DT_NODE_EXISTS(DT_NODELABEL(sw0))
+#define BUTTON0 DT_NODELABEL(sw0)
+#elif DT_NODE_EXISTS(DT_NODELABEL(button0))
+#define BUTTON0 DT_NODELABEL(button0)
+#else
+#define BUTTON0 DT_INVALID_NODE
+#endif
+
+#if DT_NODE_EXISTS(LED0)
 #define LED0_DEV DT_PHANDLE(LED0, gpios)
 #define LED0_PIN DT_PHA(LED0, gpios, pin)
 #define LED0_FLAGS DT_PHA(LED0, gpios, flags)
 
 static const struct device *led_dev = DEVICE_DT_GET(LED0_DEV);
+#endif /* LED0 */
 
-#endif
-
-#if DT_NODE_EXISTS(DT_NODELABEL(button0))
-
-#define BUTTON0 DT_NODELABEL(button0)
+#if DT_NODE_EXISTS(BUTTON0)
 #define BUTTON0_DEV DT_PHANDLE(BUTTON0, gpios)
 #define BUTTON0_PIN DT_PHA(BUTTON0, gpios, pin)
 #define BUTTON0_FLAGS DT_PHA(BUTTON0, gpios, flags)
@@ -36,12 +53,11 @@ static void button_cb(const struct device *port, struct gpio_callback *cb,
 {
 	k_work_submit(button_work);
 }
-
-#endif
+#endif /* BUTTON0 */
 
 static int led_init(void)
 {
-#if DT_NODE_EXISTS(DT_NODELABEL(led0))
+#if DT_NODE_EXISTS(LED0)
 	int err;
 
 	if (!device_is_ready(led_dev)) {
@@ -62,7 +78,7 @@ static int led_init(void)
 
 static int button_init(struct k_work *button_pressed)
 {
-#if DT_NODE_EXISTS(DT_NODELABEL(button0))
+#if DT_NODE_EXISTS(BUTTON0)
 	int err;
 
 	err = gpio_pin_configure(button_dev, BUTTON0_PIN,
@@ -104,7 +120,7 @@ int board_init(struct k_work *button_pressed)
 
 void board_led_set(bool val)
 {
-#if DT_NODE_EXISTS(DT_NODELABEL(led0))
+#if DT_NODE_EXISTS(DT_BUTTON0)
 	gpio_pin_set(led_dev, LED0_PIN, val);
 #endif
 }
