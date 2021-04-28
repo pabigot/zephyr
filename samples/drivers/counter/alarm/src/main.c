5137d47a207bc7485e90b5452107f83dd488beb5
Peter Bigot <peter.bigot@nordicsemi.no>
2021-04-28 06:43:31 -0500

    DNM: samples/driver/counter/alarm: capture diagnostic modifications
    Various changes to reproduce the issue's reported problem without
    violating the work queue API requirements and avoiding some false
    implications (e.g. that items in the work queue were not executed in
    strict FIFO order).

--- samples/drivers/counter/alarm/src/main.c
+++ samples/drivers/counter/alarm/src/main.c
@@ -2,12 +2,14 @@
 
 #include <device.h>
 #include <drivers/counter.h>
+#include <spinlock.h>
 #include <sys/printk.h>
+#include <string.h>
 
 #define DELAY 1000
 #define ALARM_CHANNEL_ID 0
 
-#if defined(CONFIG_BOARD_ATSAMD20_XPRO)
+#if defined(CONFIG_BOARD_ATSAMD20_XPRO) || defined(CONFIG_BOARD_ATSAMD21_XPRO)
 #define TIMER DT_LABEL(DT_NODELABEL(tc4))
 #elif defined(CONFIG_COUNTER_SAM_TC)
 #define TIMER DT_LABEL(DT_NODELABEL(tc0))
@@ -29,13 +31,17 @@ struct k_work_q my_work_q;
 
 struct my_data {
 	struct k_work work;
+	bool available;
+	unsigned int idx;
+	size_t reps;
 	uint32_t timestamp;
-};
+} items[10];
 
-K_MEM_SLAB_DEFINE(rx_mem_slab, sizeof(struct my_data), 10, 32);
+//K_MEM_SLAB_DEFINE(rx_mem_slab, sizeof(struct my_data), 10, 32);
 
 static void process_my_data(struct k_work *work)
 {
+	static size_t ctr;
 	struct my_data *rx_block;
 
 	rx_block = CONTAINER_OF(work, struct my_data, work);
@@ -44,28 +50,91 @@ static void process_my_data(struct k_work *work)
 	uint32_t dcycles = now - rx_block->timestamp;
 	uint32_t dtime = k_cyc_to_us_near32(dcycles);
 
-	if (dtime > 600)
-		printk("d %u\n", dtime);
+	if (dtime > 600) {
+		unsigned int idx = 0;
+#if 0
+		static struct k_spinlock lock;
+		k_spinlock_key_t key = k_spin_lock(&lock);
+		sys_snode_t *node;
+		SYS_SLIST_FOR_EACH_NODE(&my_work_q.pending, node) {
+			++idx;
+			struct k_work *wp = CONTAINER_OF(node, struct k_work, node);
+			struct my_data *mdp = CONTAINER_OF(wp, struct my_data, work);
+			mdp->idx = idx++;
 
-	k_mem_slab_free(&rx_mem_slab, (void **)&rx_block);
+		}
+		k_spin_unlock(&lock, key);
+#endif
+		printk("d%zu %u %u\n", rx_block - items, dtime, idx);
+	}
+	else if (false && ++ctr == 1000) {
+		static struct k_spinlock lock;
+		unsigned int idx = 0;
+		k_spinlock_key_t key = k_spin_lock(&lock);
+		sys_snode_t *node;
+		SYS_SLIST_FOR_EACH_NODE(&my_work_q.pending, node) {
+			struct k_work *wp = CONTAINER_OF(node, struct k_work, node);
+			struct my_data *mdp = CONTAINER_OF(wp, struct my_data, work);
+			mdp->idx = idx++;
+		}
+		k_spin_unlock(&lock, key);
+
+		printk("found %u\n", idx);
+		for (size_t i = 0; i < ARRAY_SIZE(items); ++i) {
+			struct my_data *ip = items + i;
+			printk("%zu: %02x %u %s %u %zu\n", i,
+			       k_work_busy_get(&ip->work), ip->idx,
+			       ip->available ? "avail" : "waiting",
+			       k_cyc_to_us_near32(now - ip->timestamp),
+			       ip->reps);
+		}
+		ctr = 0;
+	}
+
+	rx_block->available = true;
+	rx_block->reps += 1;
+	//k_mem_slab_free(&rx_mem_slab, (void **)&rx_block);
 }
 
 static void test_counter_interrupt_fn(const struct device *counter_dev,
 				      void *user_data)
 {
-	int ret;
-	struct my_data *rx_block;
+	static size_t li;
+	struct my_data *rx_block = NULL;
 
-	ret = k_mem_slab_alloc(&rx_mem_slab, (void **)&rx_block, K_NO_WAIT);
-	if (ret < 0) {
-		printk("Failed to allocate rx_block\n");
-		return;
+	//ret = k_mem_slab_alloc(&rx_mem_slab, (void **)&rx_block, K_NO_WAIT);
+	size_t i = li;
+	while (true) {
+		if (++i == ARRAY_SIZE(items)) {
+			i = 0;
+		}
+		if (i == li) {
+			break;
+		}
+		rx_block = items + i;
+		if (rx_block->available) {
+			if (++li == ARRAY_SIZE(items)) {
+				li = 0;
+			}
+			break;
+		}
+		rx_block = NULL;
 	}
 
+	if (!rx_block) {
+		//printk("Failed to allocate rx_block\n");
+		return;
+	}
+	//printk("alloc %zu\n", rx_block - items);
+
+	rx_block->available = false;
 	rx_block->timestamp = k_cycle_get_32();
 
-	k_work_init(&rx_block->work, process_my_data);
-	k_work_submit_to_queue(&my_work_q, &rx_block->work);
+	//k_work_init(&rx_block->work, process_my_data);
+	int rc = k_work_submit_to_queue(&my_work_q, &rx_block->work);
+	if (rc != 1) {
+		printk("submit %zu failed %d\n", rx_block - items, rc);
+	}
 }
 
 void main(void)
@@ -81,12 +150,37 @@ void main(void)
 		return;
 	}
 
+// CONFIG_NUM_COOP_PRIORITIES=16
+// _NUM_COOP_PRIORITIES=17
+// K_PRIO_COOP(x) = -(17 - x)
+// K_PRIO_COOP(15) = -2
+#define WQ_PRIO K_PRIO_COOP(15)
+	printk("prio main %d wq %d\n", CONFIG_MAIN_THREAD_PRIORITY, WQ_PRIO);
+	printk("err BUSY %d INVAL %d\n", -EBUSY, -EINVAL);
+
 	top_alarm_cfg = (struct counter_top_cfg){
 		.flags = 0,
 		.ticks = counter_us_to_ticks(counter_dev, DELAY),
 		.callback = test_counter_interrupt_fn,
 		.user_data = NULL,
 	};
+
+	memset(items, 0, sizeof(items));
+	for (size_t i = 0; i < ARRAY_SIZE(items); ++i) {
+		items[i].available = true;
+		k_work_init(&items[i].work, process_my_data);
+		printk("inited %zu\n", i);
+	}
+
+
+	struct k_work_queue_config qcfg = {
+		.no_yield = true,
+	};
+
+	k_work_queue_start(&my_work_q, my_stack,
+			   K_THREAD_STACK_SIZEOF(my_stack), WQ_PRIO,
+			   &qcfg);
+	k_yield();
 
 	ret = counter_set_top_value(counter_dev, &top_alarm_cfg);
 
@@ -103,13 +197,10 @@ void main(void)
 		printk("Error\n");
 	}
 
-	k_work_queue_start(&my_work_q, my_stack,
-			   K_THREAD_STACK_SIZEOF(my_stack), K_PRIO_COOP(15),
-			   NULL);
-
 	counter_start(counter_dev);
 
 	while (1) {
 		k_sleep(K_MSEC(1));
+		k_yield();
 	}
 }
